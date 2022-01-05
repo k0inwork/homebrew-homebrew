bae8e9d635cb0a17923d18a55b0f3b20f51799c8
k0inwork <k0inwork@gmail.com>
2022-01-06 00:45:43 +0200

    Update chntpw.rb

--- Formula/chntpw.rb
+++ Formula/chntpw.rb
@@ -5,8 +5,6 @@ class Chntpw < Formula
   sha256 "e915f5addc2673317285c6f022c94da7fdee415d9800cd38540a13706706786b"
   head "https://github.com/sidneys/chntpw.git"
 
-  depends_on "sidneys/homebrew/openssl@1.0"
-
   def install
     system "make"
     bin.install "chntpw"
